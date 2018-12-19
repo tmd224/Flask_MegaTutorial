ab3c96ea5ddd4f25d32dd417c18d717e81193354
tmd224 <41244312+tmd224@users.noreply.github.com>
2018-12-18 22:18:19 -0500

    Adding files for chapter 10

--- microblog/app/__init__.py
+++ microblog/app/__init__.py
@@ -3,6 +3,7 @@ from app.config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+from flask_mail import Mail
 import logging
 from logging.handlers import RotatingFileHandler
 import os
@@ -13,6 +14,7 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
+mail = Mail(app)
 
 from app import routes, models, errors
 
